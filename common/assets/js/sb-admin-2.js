5ac7fd7d75c9d6628156b63c6ef0d5cbb6cb1643
Benjy <benjamin.joly@live.fr>
2021-05-19 14:44:35 +0200

    fix

--- common/assets/js/sb-admin-2.js
+++ common/assets/js/sb-admin-2.js
@@ -34,26 +34,9 @@
 //     }
 //   });
 
-//   // Scroll to top button appear
-//   $(document).on('scroll', function () {
-//     var scrollDistance = $(this).scrollTop();
-//     if (scrollDistance > 100) {
-//       $('.scroll-to-top').fadeIn();
-//     } else {
-//       $('.scroll-to-top').fadeOut();
-//     }
-//   });
 
-//   // Smooth scrolling using jQuery easing
-//   $(document).on('click', 'a.scroll-to-top', function (e) {
-//     var $anchor = $(this);
-//     $('html, body').stop().animate({
-//       scrollTop: ($($anchor.attr('href')).offset().top)
-//     }, 1000, 'easeInOutExpo');
-//     e.preventDefault();
-//   });
 
-// })(jQuery); // End of use strict
+})(jQuery); // End of use strict
 
 
 
@@ -98,4 +81,24 @@ $(function () {
   $(".dp").datepicker({
     dateFormat: "dd/mm/yy",
   });
+});
+
+// Scroll to top button appear
+$(document).on('scroll', function () {
+  var scrollDistance = $(this).scrollTop();
+  if (scrollDistance > 100) {
+    $('.scroll-to-top').fadeIn();
+  } else {
+
+    $('.scroll-to-top').fadeOut();
+  }
+});
+
+// Smooth scrolling using jQuery easing
+$(document).on('click', 'a.scroll-to-top', function (e) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({
+    scrollTop: ($($anchor.attr('href')).offset().top)
+  }, 1000, 'easeInOutExpo');
+  e.preventDefault();
 });
